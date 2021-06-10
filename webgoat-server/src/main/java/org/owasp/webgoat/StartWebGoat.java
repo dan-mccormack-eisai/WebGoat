36dc966e065065dcac2e3e37ec2af0fd88f10cfb
dan-mccormack-eisai <82171410+dan-mccormack-eisai@users.noreply.github.com>
2021-06-10 14:25:34 -0500

    Try to trigger Java scan

--- webgoat-server/src/main/java/org/owasp/webgoat/StartWebGoat.java
+++ webgoat-server/src/main/java/org/owasp/webgoat/StartWebGoat.java
@@ -5,7 +5,7 @@
  * Copyright (c) 2002 - 2017 Bruce Mayhew
  * <p>
  * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the   
  * License, or (at your option) any later version.
  * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
@@ -22,6 +22,8 @@
  * projects.
  * <p>
  */
+
+// test
 
 package org.owasp.webgoat;
 
